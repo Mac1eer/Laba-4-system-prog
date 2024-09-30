21a7614ae0feb275c11e883a9f230a7187dbd4bb
Mac1eer <147583135+Mac1eer@users.noreply.github.com>
2024-09-30 09:03:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число ваших денег");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 1;
+int c = a / 3;
+Console.WriteLine($"По акции берете 3 шоколадки - 1 бесплатно вы получаете {a + c} шоколадки");
+
